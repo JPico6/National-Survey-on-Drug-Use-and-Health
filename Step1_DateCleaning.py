199935d4a39f2202322f815526a47abc905fe846
JPico6 <JPico6@users.noreply.github.com>
2016-09-30 18:47:40 -0400

    Update Step1_DateCleaning.py

--- Step1_DateCleaning.py
+++ Step1_DateCleaning.py
@@ -33,8 +33,6 @@ df['CIGEVER'].unique()
 df['CIGEVER'].value_counts()
 df['CIGTRY'].value_counts()
 
-#...and so on...
-
 ##########Select Potentially Useful Features##########
 ############################################################
 
@@ -81,167 +79,69 @@ df1 = pd.read_csv("C:/Users/piccone/Desktop/DS projects/national survey on drug 
 ############################################################
 
 '''Significant recoding is necessary:
-(1) Convert some values to missing
-(2) Other extraneous recoding
 
-(a)
-convert LSD, PCP, PEYOTE, PSILCY, ECSTASY, AMYLNIT, CLEFLU, GAS, GLUE, ETHER, NITOXID,
-    AEROS, DARVTYLC, PERCTYLX, VICOLOR, HYDROCOD, METHDON, MORPHINE, OXYCONTN, KLONOPIN 
-    METHDES, DIETPILS, RITMPHEN,  MTHAMP, BOOKED, values of 3 (logically assigned "yes") to 1 ("yes")
-'''
-df1['LSD'][df1.LSD == 3] = 1
-df1['PCP'][df1.PCP == 3] = 1
-df1['PEYOTE'][df1.PEYOTE == 3] = 1
-df1['PSILCY'][df1.PSILCY == 3] = 1
-df1['AMYLNIT'][df1.AMYLNIT == 3] = 1
-df1['CLEFLU'][df1.CLEFLU == 3] = 1
-df1['GAS'][df1.GAS == 3] = 1
-df1['GLUE'][df1.GLUE == 3] = 1
-df1['ETHER'][df1.ETHER == 3] = 1
-df1['NITOXID'][df1.NITOXID == 3] = 1
-df1['AEROS'][df1.AEROS == 3] = 1
-df1['DARVTYLC'][df1.DARVTYLC == 3] = 1
-df1['PERCTYLX'][df1.PERCTYLX == 3] = 1
-df1['VICOLOR'][df1.VICOLOR == 3] = 1
-df1['HYDROCOD'][df1.HYDROCOD == 3] = 1
-df1['METHDON'][df1.METHDON == 3] = 1
-df1['MORPHINE'][df1.MORPHINE == 3] = 1
-df1['OXYCONTN'][df1.OXYCONTN == 3] = 1
-df1['KLONOPIN'][df1.KLONOPIN == 3] = 1
-df1['METHDES'][df1.METHDES == 3] = 1
-df1['DIETPILS'][df1.DIETPILS == 3] = 1
-df1['RITMPHEN'][df1.RITMPHEN == 3] = 1
-df1['MTHAMP'][df1.MTHAMP == 3] = 1
-df1['BOOKED'][df1.BOOKED == 3] = 1
+(a) Convert the following variables from 3 (logically assigned "yes") to 1 ("yes)'''
+vars1 = ['LSD', 'PCP', 'PEYOTE', 'PSILCY', 'ECSTASY', 'AMYLNIT', 'CLEFLU', 'GAS', 'GLUE', 'ETHER', 'NITOXID',
+    'AEROS', 'DARVTYLC', 'PERCTYLX', 'VICOLOR', 'HYDROCOD', 'METHDON', 'MORPHINE', 'OXYCONTN', 'KLONOPIN', 
+    'METHDES', 'DIETPILS', 'RITMPHEN',  'MTHAMP', 'BOOKED']
+for col in vars1:
+    df1.loc[df1[col] == 3, col] = 1
 
-'''(b)
-MORPHINE, OXYCONTN, HYDROCOD, METHDON    6 ("response not entered") = NaN'''
-df1['MORPHINE'][df1.MORPHINE == 6] = np.nan
-df1['OXYCONTN'][df1.OXYCONTN == 6] = np.nan
-df1['HYDROCOD'][df1.HYDROCOD == 6] = np.nan
-df1['METHDON'][df1.METHDON == 6] = np.nan
+'''(b) Convert 6 ("response not entered") = NaN'''
+vars2 = ['MORPHINE', 'OXYCONTN', 'HYDROCOD', 'METHDON']
+for col in vars2:
+    df1.loc[df1[col] == 6, col] = np.nan
+    
 
-'''(c)
-ANLNOLST, STMNOLST, MTHAMP1  4 ("No, logically assigned") =2 ("no")'''
-df1['ANLNOLST'][df1.ANLNOLST == 4] = 2
-df1['STMNOLST'][df1.STMNOLST == 4] = 2
-df1['MTHAMP'][df1.MTHAMP == 4] = 2
+'''(c) Convert 4 ("No, logically assigned") =2 ("no")'''
+vars3 = ['ANLNOLST', 'STMNOLST', 'MTHAMP'] 
+for col in vars3:
+    df1.loc[df1[col] == 4, col] = 2
+  
+'''(d) Convert 81 ("never used X type of drug, logically assigned" and 91 ("never used X type of drug") = 2 ("no")'''
+vars4 = ['TRNEVER', 'STMEVER', 'SEDEVER'] 
+for col in vars4:
+    df1.loc[df1[col] == 81, col] = 2
+for col in vars4:
+    df1.loc[df1[col] == 91, col] = 2
+ 
+'''(e) Convert 6 ("not entered") = 2 ("No"), everythng else = NaN'''
+vars5 = ['LIFANXD', 'LIFASMA', 'LIFBRONC', 'LIFDEPRS', 'LIFDIAB', 'LIFHARTD', 'LIFHBP', 'LIFHIV', 'LIFPNEU',
+     'LIFSTDS', 'LIFSINUS', 'LIFSLPAP', 'YETLKNON', 'YETLKPAR', 'YETLKBGF', 'YETLKOTA', 'YETLKSOP'] 
+for col in vars5:
+    df1.loc[df1[col] == 6, col] = 2
 
-'''(d)*******************************
-TRNEVER, STMEVER, SEDEVER  81 ("never used X type of drug, logically assigned" and 91 ("never used X type of drug") = 2 ("no")'''
-df1['TRNEVER'][df1.TRNEVER == 81] = 2
-df1['TRNEVER'][df1.TRNEVER == 91] = 2
-df1['STMEVER'][df1.STMEVER == 81] = 2
-df1['STMEVER'][df1.STMEVER == 91] = 2
-df1['SEDEVER'][df1.SEDEVER == 81] = 2
-df1['SEDEVER'][df1.SEDEVER == 91] = 2
-
-'''(e)
-LIFANXD, LIFASMA, LIFBRONC, LIFDEPRS, LIFDIAB, LIFHARTD, LIFHBP, LIFHIV, LIFPNEU,
-     LIFSTDS, LIFSINUS, LIFSLPAP, YETLKNON1, YETLKPAR1, YETLKBGF1, YETLKOTA1, YETLKSOP1, 6 ("not entered") = 2 ("No"), everythng else = NaN'''
-df1['LIFANXD'][df1.LIFANXD == 6] = 2
-df1['LIFASMA'][df1.LIFASMA == 6] = 2
-df1['LIFBRONC'][df1.LIFBRONC == 6] = 2
-df1['LIFDEPRS'][df1.LIFDEPRS == 6] = 2
-df1['LIFDIAB'][df1.LIFDIAB == 6] = 2
-df1['LIFHARTD'][df1.LIFHARTD == 6] = 2
-df1['LIFHBP'][df1.LIFHBP == 6] = 2
-df1['LIFHIV'][df1.LIFHIV == 6] = 2
-df1['LIFPNEU'][df1.LIFPNEU == 6] = 2
-df1['LIFSTDS'][df1.LIFSTDS == 6] = 2
-df1['LIFSINUS'][df1.LIFSINUS == 6] = 2
-df1['LIFSLPAP'][df1.LIFSLPAP == 6] = 2
-df1['YETLKNON'][df1.YETLKNON == 6] = 2
-df1['YETLKPAR'][df1.YETLKPAR == 6] = 2
-df1['YETLKBGF'][df1.YETLKBGF == 6] = 2
-df1['YETLKOTA'][df1.YETLKOTA == 6] = 2
-df1['YETLKSOP'][df1.YETLKSOP == 6] = 2
-
-'''(f)
-YELSTGRD and IMPRESP > 4 (multiple missing values) = NaN'''
-df1['YELSTGRD'][df1.YELSTGRD > 4] = np.nan
-df1['IMPRESP'][df1.IMPRESP > 4] = np.nan
-
-'''(g)
-NRCH17_2 -9 ("missing") = NaN'''
+'''(f) Convert > 4 (multiple missing values) = NaN'''
+vars6 = ['YELSTGRD', 'IMPRESP']
+for col in vars6:
+    df1.loc[df1[col] > 4, col] = np.nan
+ 
+'''(g) Convert -9 ("missing") = NaN'''
 df1['NRCH17_2'][df1.NRCH17_2 == -9] = np.nan
 
-'''(h)
-SCHENRL 3, 5, 11 ("Yes, logically assigned") = yes(1)'''
+'''(h) Convert SCHENRL 3, 5, 11 ("Yes, logically assigned") = yes(1)'''
 df1['SCHENRL'][df1.SCHENRL == 3] = 1
 df1['SCHENRL'][df1.SCHENRL == 5] = 1
 df1['SCHENRL'][df1.SCHENRL == 11] = 1
 
-'''(i)
-Change a few others to no...
-CRKEVER, LSD, PCP, PEYOTE, MESC, PSILCY, ECSTASY, HALNOLST, AMYLNIT, CLEFLU, GAS, GLUE, ETHER,
-  SOLVENT, LGAS, NITOXID, SPPAINT, AEROS, INHNOLST, PERCTYLX, VICOLOR, HYDROCOD, METHDON, DARVTYLC,
-  MORPHINE, OXYCONTN, ANLNOLST, KLONOPIN, XNAXATVN, VALMDIAZ, TRNEVER, METHDES, DIETPILS, 
-  RITMPHEN, STMNOLST, STMEVER, SEDEVER,  91 ("Ever used") -> 2 ("no") '''
+'''(i) Convert 91 ("Ever used") -> 2 ("no") '''
+vars7 = ['CRKEVER', 'LSD', 'PCP', 'PEYOTE', 'MESC', 'PSILCY', 'ECSTASY', 'HALNOLST', 'AMYLNIT', 'CLEFLU', 'GAS', 'GLUE', 'ETHER',
+  'SOLVENT', 'LGAS', 'NITOXID', 'SPPAINT', 'AEROS', 'INHNOLST', 'PERCTYLX', 'VICOLOR', 'HYDROCOD', 'METHDON', 'DARVTYLC',
+  'MORPHINE', 'OXYCONTN', 'ANLNOLST', 'KLONOPIN', 'XNAXATVN', 'VALMDIAZ', 'TRNEVER', 'METHDES', 'DIETPILS', 
+  'RITMPHEN', 'STMNOLST', 'STMEVER', 'SEDEVER']  
+for col in vars7:
+    df1.loc[df1[col] == 91, col] = 2  
   
-df1['CRKEVER'][df1.CRKEVER == 91] = 2
-df1['LSD'][df1.LSD == 91] = 2
-df1['PCP'][df1.PCP == 91] = 2
-df1['PEYOTE'][df1.PEYOTE == 91] = 2
-df1['MESC'][df1.MESC == 91] = 2
-df1['PSILCY'][df1.PSILCY == 91] = 2
-df1['ECSTASY'][df1.ECSTASY == 91] = 2
-df1['HALNOLST'][df1.HALNOLST == 91] = 2
-df1['AMYLNIT'][df1.AMYLNIT == 91] = 2
-df1['CLEFLU'][df1.CLEFLU == 91] = 2
-df1['GAS'][df1.GAS == 91] = 2
-df1['GLUE'][df1.GLUE == 91] = 2
-df1['ETHER'][df1.ETHER == 91] = 2
-df1['SOLVENT'][df1.SOLVENT == 91] = 2
-df1['LGAS'][df1.LGAS == 91] = 2
-df1['NITOXID'][df1.NITOXID == 91] = 2
-df1['SPPAINT'][df1.SPPAINT == 91] = 2
-df1['AEROS'][df1.AEROS == 91] = 2
-df1['INHNOLST'][df1.INHNOLST == 91] = 2
-df1['PERCTYLX'][df1.PERCTYLX == 91] = 2
-df1['VICOLOR'][df1.VICOLOR == 91] = 2
-df1['HYDROCOD'][df1.HYDROCOD == 91] = 2
-df1['METHDON'][df1.METHDON == 91] = 2
-df1['DARVTYLC'][df1.DARVTYLC == 91] = 2
-df1['MORPHINE'][df1.MORPHINE == 91] = 2
-df1['OXYCONTN'][df1.OXYCONTN == 91] = 2
-df1['ANLNOLST'][df1.ANLNOLST == 91] = 2
-df1['KLONOPIN'][df1.KLONOPIN == 91] = 2
-df1['XNAXATVN'][df1.XNAXATVN == 91] = 2
-df1['VALMDIAZ'][df1.VALMDIAZ == 91] = 2
-df1['TRNEVER'][df1.TRNEVER == 91] = 2
-df1['METHDES'][df1.METHDES == 91] = 2
-df1['DIETPILS'][df1.DIETPILS == 91] = 2
-df1['RITMPHEN'][df1.RITMPHEN == 91] = 2
-df1['STMNOLST'][df1.STMNOLST == 91] = 2
-df1['STMEVER'][df1.STMEVER == 91] = 2
-df1['SEDEVER'][df1.SEDEVER == 91] = 2
+'''Convert 81 ("Never used X type of drug")-> 2("No")'''
+vars8 = ['METHDON', 'VICOLOR', 'HYDROCOD', 'PERCTYLX', 'DARVTYLC', 'MORPHINE', 'OXYCONTN', 'ANLNOLST', 'KLONOPIN', 'XNAXATVN',
+    'VALMDIAZ', 'TRNEVER', 'METHDES', 'DIETPILS', 'RITMPHEN', 'STMNOLST', 'STMEVER', 'SEDEVER']      
+for col in vars8:
+    df1.loc[df1[col] == 81, col] = 2
 
-'''METHDON, VICOLOR, HYDROCOD, PERCTYLX, DARVTYLC, MORPHINE, OXYCONTN, ANLNOLST, KLONOPIN, XNAXATVN,
-    VALMDIAZ, TRNEVER, METHDES, DIETPILS, RITMPHEN, STMNOLST, STMEVER, SEDEVER,      81 ("Never used X type of drug")-> 2("No")'''
-df1['METHDON'][df1.METHDON == 81] = 2
-df1['VICOLOR'][df1.VICOLOR == 81] = 2
-df1['HYDROCOD'][df1.HYDROCOD == 81] = 2
-df1['PERCTYLX'][df1.PERCTYLX == 81] = 2
-df1['DARVTYLC'][df1.DARVTYLC == 81] = 2
-df1['MORPHINE'][df1.MORPHINE == 81] = 2
-df1['OXYCONTN'][df1.OXYCONTN == 81] = 2
-df1['ANLNOLST'][df1.ANLNOLST == 81] = 2
-df1['KLONOPIN'][df1.KLONOPIN == 81] = 2
-df1['XNAXATVN'][df1.XNAXATVN == 81] = 2
-df1['VALMDIAZ'][df1.VALMDIAZ == 81] = 2
-df1['TRNEVER'][df1.TRNEVER == 81] = 2
-df1['METHDES'][df1.METHDES == 81] = 2
-df1['DIETPILS'][df1.DIETPILS == 81] = 2
-df1['RITMPHEN'][df1.RITMPHEN == 81] = 2
-df1['STMNOLST'][df1.STMNOLST == 81] = 2
-df1['STMEVER'][df1.STMEVER == 81] = 2
-df1['SEDEVER'][df1.SEDEVER == 81] = 2
-
-'''(j)
-Convert all other 80+ to Nan'''
+'''(j) Convert all other 80+ to Nan'''
 df1[df1 > 80] = np.nan
 
+#save to file
 df1.to_csv('df2.csv', index=False)  
 df2 = pd.read_csv("C:/Users/piccone/Desktop/DS projects/national survey on drug use and health 2012/ICPSR_34933/DS0001/df2.csv")
 
